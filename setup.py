b8f24f28614e9b922faf552094109927f7e73bce
sralex <alex_machine2@hotmail.com>
2021-03-15 22:43:32 -0600

    Update h5py version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='clean_speech',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),#['speech_enhance'],
     package_data={'clean_speech': ['*.json','*.h5']},
     include_package_data=True,
@@ -11,7 +11,8 @@ setup(
     	"scipy>=1.5.2",
         "tensorflow==1.5.0",
         "keras==2.2.3",
-        "soundfile>=0.10.3.post1"
+        "soundfile>=0.10.3.post1",
+        "h5py==2.9",
       ],
     zip_safe=False
 )
